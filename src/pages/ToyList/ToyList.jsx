2090a467c167d1f36caa640b0cc9941ccd7ac784
whlong1 <whunterlong@gmail.com>
2022-04-03 17:57:11 -0400

    update toylist header

--- src/pages/ToyList/ToyList.jsx
+++ src/pages/ToyList/ToyList.jsx
@@ -1,13 +1,24 @@
 import './ToyList.css'
 
+// Image Assets
+import Fish from '../../assets/fish.svg'
+import Mouse from '../../assets//mouse.svg'
+import StringToy from '../../assets/string.svg'
+import PostToy from '../../assets/post.svg'
+
 // Components
 import ToyCard from '../../components/ToyCard/ToyCard'
-import PageHeader from '../../components/PageHeader/PageHeader'
 
 const ToyList = (props) => {
   return (
     <>
-      <PageHeader title="All Cat Toys" />
+      <section className="page-header">
+        <h1>Toy List</h1>
+        <img src={Mouse} alt="a mouse toy" />
+        <img src={Fish} alt="a fish toy" />
+        <img src={StringToy} alt="a string toy" />
+        <img src={PostToy} alt="a post toy" />
+      </section>
       <section className="toy-card-container">
         {props.toys.map((toy) => (
           <ToyCard key={toy.id} toy={toy} isCard={true} />
